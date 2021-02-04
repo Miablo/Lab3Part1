db1273a0e3862da0f698246bcbc001d85c604889
miablo <mvd5044@psu.edu>
2021-02-04 17:08:22 -0500

    added instructions

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    /*
+    (5 pts) Given the preconditions for x and y as |x – y| ≤ 200, |2x + y - 150| ≤ 250,
+    show the post-conditions for z = 3x2 + y2 + 3xy – 7x – 8y + 10.
+    Please solve this problem mathematically and draw a 2D graph to show the covered domain for x and y.
+     */
+}
